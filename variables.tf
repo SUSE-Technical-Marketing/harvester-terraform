73eb6c0438cc31f4d6eddc764661f6a9dd7081c0
Andrés Valero <anvarui@gmail.com>
2022-08-11 11:54:02 +0200

    mre examples added

--- variables.tf
+++ variables.tf
@@ -1,1 +1,8 @@
 # In this file you'll define variables you are going to use in terraform
+
+# variable definition example, NOT USED for Harvester example
+#variable "aws_region" {
+#  description = "AWS region"
+#  type        = string
+#  default     = "us-west-2"
+#}
