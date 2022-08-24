f0dc370d0232699dd1d6bb135c627526c24635a9
juanbrny <juan@brainy.es>
2022-08-24 17:51:05 +0200

    Clean up images file

--- images.tf
+++ images.tf
@@ -1,18 +1,4 @@
-## IMAGES to be present in Harvester ##
-
-##Use data instead of resource to reuse images!!!
-
-# resource "harvester_image" "opensuse" {
-#   name         = "opensuse"
-#   namespace    = "default"
-#   display_name = "opensuse"
-#   source_type  = "download"
-
-#   #url = "https://download.opensuse.org/distribution/leap/15.4/appliances/openSUSE-Leap-15.4-JeOS.x86_64-kvm-and-xen.qcow2"
-#   #url = "http://192.168.200.19/iso/sles15-sp3.qcow2"
-#   url = "https://download.opensuse.org/repositories/Cloud:/Images:/Leap_15.4/images/openSUSE-Leap-15.4.x86_64-NoCloud.qcow2"
-# }
-
+## Image to be created in Harvester ##
 resource "harvester_image" "os_image" {
   name         = var.os_image.name
   namespace    = var.os_image.namespace
@@ -22,12 +8,3 @@ resource "harvester_image" "os_image" {
   tags         = var.os_image.tags
 }
 
-#resource "harvester_image" "ubuntu20" {
-#  name         = "ubuntu20"
-#  namespace    = "default"
-#  display_name = "ubuntu20"
-#  source_type  = "download"
-#  url          = "http://cloud-images.ubuntu.com/releases/focal/release/ubuntu-20.04-server-cloudimg-amd64.img"
-#}
-
-#https://download.opensuse.org/repositories/Cloud:/Images:/Leap_15.4/images/openSUSE-Leap-15.4.x86_64-NoCloud.qcow2
